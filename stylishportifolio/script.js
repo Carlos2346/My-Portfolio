279ae1afb361b9ad0579509247ed13ff2048d7f2
Carlos2346 <128575543+Carlos2346@users.noreply.github.com>
2023-03-29 11:07:30 +0200

    Delete stylishportifolio directory

--- stylishportifolio/script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon=document.querySelector('.hamburger-menu');
-const navbar=document.querySelector('.navbar');
-
-
-
-
-    menuIcon.addEventListter('.click' ,()=>{
-        navbar.classList.toggle("change")
-})
