08ca54082015bb3a08f34b8d187dab9be36efd6d
Carlos2346 <128575543+Carlos2346@users.noreply.github.com>
2023-03-29 09:22:56 +0200

    Add files via upload

---
+++ stylishportifolio/script.js
@@ -0,0 +1,9 @@
+const menuIcon=document.querySelector('.hamburger-menu');
+const navbar=document.querySelector('.navbar');
+
+
+
+
+    menuIcon.addEventListter('.click' ,()=>{
+        navbar.classList.toggle("change")
+})
